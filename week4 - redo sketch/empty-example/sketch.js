bb011c4de2c3d6060586efca452da0af54c141c9
stormhalestrap <hales275@newschool.edu>
2020-02-18 05:14:41 +0000

    week 4

---
+++ week4 - redo sketch/empty-example/sketch.js
@@ -0,0 +1,76 @@
+var xPos = -500;
+var yPos = 100;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  //background(220);
+  xPos = xPos + 10;
+  yPos = yPos + 10;
+  fill("#1569C7");
+  fill("#1569C7");
+  if (yPos >= 200){
+    yPos = 0;
+    fill("#0000A0");
+  rect(xPos, yPos, 17890000, 1000);
+  }
+  
+  //rect(mouseX, mouseY, 70,70);
+    //fill("1569C7");
+  //rect(mouseX, mouseY, 65,65);
+   //fill("#0000A0");
+  //rect(mouseX, mouseY, 60,60);
+  //fill("blue")
+  //rect(mouseX, mouseY, 55,55);
+  //fill("1569C7");
+  //rect( 50,50);
+  //fill("#0000A0");
+  //rect( 45,45);
+  //rect(mouseX, mouseY, 40,40);
+  //rect(mouseX, mouseY, 35,35);
+  //rect(mouseX, mouseY, 30,30);
+  //rect(mouseX, mouseY, 25,25);
+  //rect(mouseX, mouseY, 20,20);
+  //rect(mouseX, mouseY, 15,15);
+  //rect(mouseX, mouseY, 10,10);
+  //rect(mouseX, mouseY, 5,5);
+  //rect(mouseX, mouseY, 0,0);
+  
+  
+   xPos = xPos + 10;
+  yPos = yPos + 10;
+  fill("#1569C7");
+  fill("#1569C7");
+  if (yPos >= 200){
+    yPos = 0;
+    fill("#0000A0");
+  rect(xPos, yPos, 170, 1000);
+    
+  }
+  
+  rect(xPos, yPos, 70,70);
+    fill("1569C7");
+  rect(xPos, yPos, 65,65);
+   fill("#0000A0");
+  rect(xPos, yPos, 60,60);
+  fill("blue")
+  rect(xPos, yPos, 55,55);
+  fill("1569C7");
+  rect( 50,50);
+  fill("#0000A0");
+  rect( 45,45);
+  rect(xPos, yPos, 40,40);
+  rect(xPos, yPos, 35,35);
+  rect(xPos, yPos, 30,30);
+  rect(xPos, yPos, 25,25);
+  rect(xPos, yPos, 20,20);
+  rect(xPos, yPos, 15,15);
+  rect(xPos, yPos, 10,10);
+  rect(xPos, yPos, 5,5);
+  rect(xPos, yPos, 0,0);
+  
+  
+  
+}
